461637fb9d5e5ed95c331df2a2436138e92eee2c
Chris Karampagias <karampagiaschris@gmail.com>
2024-05-13 21:31:50 +0300

    Create HTML structure, style app

--- tic-tac-toe/logic.js
+++ tic-tac-toe/logic.js
@@ -1,1 +1,20 @@
-const x = 10;
+const boardCells = document.querySelectorAll(".cell");
+
+function setCellBorders() {
+  boardCells.forEach((cell, index) => {
+    console.log(index);
+
+    if ([0, 1, 2].includes(index)) {
+      cell.style.borderBottom = "1px solid rgb(117, 117, 117)";
+    } else if ([6, 7, 8].includes(index)) {
+      cell.style.borderTop = "1px solid rgb(117, 117, 117)";
+    }
+
+    if ([1, 4, 7].includes(index)) {
+      cell.style.borderLeft = "1px solid rgb(117, 117, 117)";
+      cell.style.borderRight = "1px solid rgb(117, 117, 117)";
+    }
+  });
+}
+
+setCellBorders();
