c267ecccb079f4c39d76039321cf073537a884cb
Chris Karampagias <karampagiaschris@gmail.com>
2024-05-19 19:44:42 +0300

    Create functional component version

--- twitter-card/src/App.jsx
+++ twitter-card/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TwitterCardClass } from "./TwitterCardClass";
+import { TwitterCardFunctional } from "./TwitterCardFunctional";
+import "./App.css";
+
+const url = {
+  target: "https://x.com/?lang=en",
+};
+
+url.label = url.target.split("https://")[1].split("/")[0];
+
+const twitterCardProps = {
+  user: "Twitter",
+  description: `Happy 3rd anniversary #TBT! See how &quot;Throwback Thursday&quot;
+  cemented its status as a weekly Twitter tradition:
+  blog.twitter.com/2015/history-o...`,
+  title: "History of #TBT on Twitter",
+  subtitle: "History of #TBT on Twitter",
+  url: url,
+  numOfHearts: 695,
+  numOfComments: 482,
+  dateCreated: "6:26 PM - Apr 30, 2015",
+  commentUrl: "https://x.com/?lang=en",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TwitterCardFunctional {...twitterCardProps} />
+      <TwitterCardClass />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ twitter-card/src/TwitterCardClass/index.js
@@ -0,0 +1,1 @@
+export { default as TwitterCardClass } from "./TwiiterCardClass";
