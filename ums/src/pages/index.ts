e3f321e960a2a28f901c9fa39888a6ae38a0e19e
Chris Karampagias <karampagiaschris@gmail.com>
2024-05-28 23:25:47 +0300

    Create app page structure

---
+++ ums/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as AllUsers } from "./AllUsers";
+export { default as ChangePassword } from "./ChangePassword";
+export { default as Homepage } from "./Homepage";
+export { default as Authentication } from "./Authentication";
