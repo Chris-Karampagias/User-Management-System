841cfbf60072d4806896941b703c78e7b51eae78
Chris Karampagias <karampagiaschris@gmail.com>
2024-06-10 20:05:04 +0300

    Add mutation for creating a user, enable query just with username (for checking user existance through api call)

--- ums/src/queries/useUser.js
+++ ums/src/queries/useUser.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "../api";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { getUser, createUser } from "../api";
 
 export const useUser = () => {
   const [innerUsername, setInnerUserName] = useState("");
-  const [innerPassword, setInnerPassword] = useState("");
+  const [innerPassword, setInnerPassword] = useState(null);
 
-  const userRefetch = (username, password) => {
+  const userRefetch = (username, password = null) => {
     setInnerUserName(username);
     setInnerPassword(password);
   };
@@ -18,9 +18,13 @@ export const useUser = () => {
   } = useQuery({
     queryKey: ["user", innerUsername, innerPassword],
     queryFn: () => getUser(innerUsername, innerPassword),
-    enabled: !!innerUsername && !!innerPassword,
+    enabled: !!innerUsername,
     refetchOnWindowFocus: false,
   });
+
+  const { isPending: isCreatingUser, mutate: createUserFromData } = useMutation(
+    { mutationFn: createUser }
+  );
 
   return {
     user: user?.length > 0 ? user[0] : null,
@@ -28,5 +32,7 @@ export const useUser = () => {
     userIsError: !!user,
     userIsFetched,
     userRefetch,
+    isCreatingUser,
+    createUserFromData,
   };
 };
