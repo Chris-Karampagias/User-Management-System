6e283542a2e4321f2ce765c3abb680fdea518ed2
Chris Karampagias <karampagiaschris@gmail.com>
2024-07-28 20:43:52 +0300

    feat: add loading spinner while fetching users and error reporting

--- ums/src/pages/AllUsers/AllUsers.jsx
+++ ums/src/pages/AllUsers/AllUsers.jsx
@@ -1,5 +1,7 @@
-import { Stack, Typography, Paper } from "@mui/material";
+import { Stack, Typography, Paper, CircularProgress } from "@mui/material";
+import { useSelector } from "react-redux";
 import { UserPreviewCard } from "./components";
+import { userIdSelector } from "../../models/user/selectors";
 import { useUsers } from "../../queries/useUsers";
 import { PaginatedList } from "../../components";
 
@@ -15,14 +17,32 @@ const renderItem = (user) => {
 };
 
 export function AllUsers() {
-  const { users, isLoadingUsers } = useUsers();
+  const { users, isLoadingUsers, loadingUsersFailed } = useUsers();
+  const userId = useSelector(userIdSelector);
+
+  const usersWithoutCurrentAdmin = users.filter((user) => userId !== user.id);
   return (
     <Paper elevation={2} sx={{ padding: "20px 10px" }}>
       <Stack direction="column" gap={3}>
         <Typography alignSelf="center" variant="h1" fontSize={30}>
           All Users
         </Typography>
-        <PaginatedList items={users} renderItem={renderItem}/>
+        {isLoadingUsers ? (
+          <CircularProgress sx={{ alignSelf: "center" }} size={60} />
+        ) : (
+          <>
+            {loadingUsersFailed ? (
+              <Typography textAlign="center" fontSize={20} color="error">
+                Unable to retrieve users data. Please try again later.
+              </Typography>
+            ) : (
+              <PaginatedList
+                items={usersWithoutCurrentAdmin}
+                renderItem={renderItem}
+              />
+            )}
+          </>
+        )}
       </Stack>
     </Paper>
   );
